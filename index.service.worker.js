ec3d88ccaa09108bdd00538c60a63328566aae08
Lynla_13 <115152782+Lynla13@users.noreply.github.com>
2026-06-14 22:42:25 +0700

    v 0.1.5
    Add some items, playable Haku

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781366366|226738334';
+const CACHE_VERSION = '1781451701|374303898';
 /** @type {string} */
 const CACHE_PREFIX = 'hakushine-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
